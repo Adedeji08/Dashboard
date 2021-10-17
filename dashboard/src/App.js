08c29e6945dce20e5f89045d426a880e120d5f17
Damilola Adedeji <ebenoluwadamilola@gmail.com>
2021-10-17 21:07:37 +0100

    first commit

---
+++ dashboard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './Challenge/Dashboard';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Dashboard />  
+    </div>
+  );
+};
+
+export default App;
---
+++ dashboard/src/Challenge/Dashboard.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Dashboard.css';
+import { useState, useEffect } from 'react';
+
+      
+/* export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+              <div className='dash'>
+              <br /><br /><br /><br /><br />
+                  <div className='dash_second'>
+                  <img src='./image-jeremy.png' className='icon' alt="fireSpot"/>
+                  <h6 className='for'>Report for</h6>
+                  <h2>Jeremy Robson</h2>
+                  <ul className='list' type='none'>
+                      <li>Daily</li><br />
+                      <li>Weekly</li> <br />
+                      <li>Monthly</li><br/>
+                  </ul>
+                   </div> 
+                   <div className='spc'>''</div>
+                   
+                   <div className='side'>
+                     <h5>Work</h5>
+                     <h1>32hrs</h1>
+                     <h5>Last Week-36hrs</h5>
+                  </div>
+                  <div className='spc'>''</div>
+                  <div className='side'>
+                     <h5>Play</h5>
+                     <h1>10hrs</h1>
+                     <h5>Last Week-8hrs</h5>
+                  </div>
+                  <div className='spc'>''</div>
+                  <div className='side'>
+                     <h5>Study</h5>
+                     <h1>4hrs</h1>
+                     <h5>Last Week-7hrs</h5>
+                  </div>
+                  <div className='gap' style={{width: '60%', color: 'whitesmoke'}}>''</div>
+                  <div className='spc'>''</div>
+                  <div className='side'>
+                     <h5>Exercise</h5>
+                     <h1>4hrs</h1>
+                     <h5>Last Week-5hrs</h5>
+                  </div>
+                  <div className='spc'>''</div>
+                  <div className='side'>
+                     <h5>Social</h5>
+                     <h1>5hrs</h1>
+                     <h5>Last Week-10hrs</h5>
+                  </div>
+                  <div className='spc'>''</div>
+                  <div className='side'>
+                     <h5>Self Care</h5>
+                     <h1>2hrs</h1>
+                     <h5>Last Week-2hrs</h5>
+                  </div>
+              </div>
+            </div>
+        )
+    }
+} */
